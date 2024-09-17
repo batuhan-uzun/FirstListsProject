bcf8c2e2dd314559e614ce78f76adbc30f70a6ff
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-09-18 02:13:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿List<string> invitees = new List<string>(); // List of type String was created
+
+invitees.Add("Bülent Ersoy");
+invitees.Add("Ajda Pekkan");
+invitees.Add("Ebru Gündeş");
+invitees.Add("Hadise");
+invitees.Add("Hande Yener");
+invitees.Add("Tarkan");
+invitees.Add("Funda Arar");
+invitees.Add("Demet Akalın");
+
+foreach (string invite in invitees) // Names in invitees were printed on the screen with a forach
+{
+    Console.WriteLine(invite);
+}
